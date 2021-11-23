a3da01532a71e76202bdcad5698be14cd95a8724
EC2 Default User <ec2-user@ip-172-31-76-146.ec2.internal>
2021-11-23 08:07:16 +0000

    []

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user=@book.user
+    @book_new = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to book_path(@book.id)
+    flash[:notice] = "You have created book successfully."
+    else
+     @user = current_user
+     @books = Book.all
+     render("books/index")
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    flash[:notice] = "You have updated book successfully."
+    else
+     render("books/edit")
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    redirect_to books_path unless current_user.id == @book.user_id
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+end
---
+++ bookers2/app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user=current_user
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to user_path(@user.id)
+    flash[:notice] = "You have updated user successfully."
+    else
+     render("users/edit")
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    # if @user = current_user
+    #   render ("users/edit")
+    # else
+    #   redirect_to books_path
+    # end
+      redirect_to user_path(current_user) unless current_user.id == @user.id
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+
+end
